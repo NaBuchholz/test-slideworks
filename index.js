c213deb70227c2b0671997733d0e74187877c7ea
NaBuchholz <nathaliabuchh@hotmail.com>
2023-05-17 11:42:17 -0300

    💩 Primeira tentativa certa

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 require('dotenv').config();
-const express = require('express')
-const fs = require('fs')
-const app = express()
-const port = 3000
-
+const express = require('express');
+const fetch = require('node-fetch');
+const fs = require('fs');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-
-	let json = JSON.parse(fs.readFileSync(process.env.URL))
-	let response = `<html><head><title>Desafio</title></head><body>`
-
-	json.forEach((i, item) => {
-		if ( i <= 100 ){
-			response = `<h3>${item.albumId} / ${item.id} - ${item.title}</h3><img src='${item.url}'><br />`
-		}
-	})
-
-	response += `</body>`
-
-	res.send(response)
-})
+	fetch(process.env.URL)
+		.then((resp) => {
+			return resp.json();
+		})
+		.then((data) => {
+			console.log(data);
+			let response = `<!DOCTYPE html><html><head><title>Desafio</title></head><body>${data.map(
+				(item) => {
+					if (item.id <= 100) {
+						return `<h3>${item.albumId} / ${item.id} - ${item.title}</h3><img src='${item.url}'/><br />`;
+					}
+				}
+			)}`;
+			response = response.concat('</body></html>');
+			fs.writeFile('Movies.html', response, (err) => {
+				if (err) throw err;
+				console.log('arquivo escrito okay');
+			});
+			res.end(response);
+		})
+		.catch((error) => {
+			console.log('Erro ao obter os dados:', error);
+			res.status(500).send('Erro ao obter os dados');
+		});
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+	console.log(`Example app listening at http://localhost:${port}`);
+});
